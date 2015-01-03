98ade6f4756b3246809deb7bacbb43f95ef49a3c
HyperSprite <github@hypersprite.com>
2015-01-03 06:53:57 -0800

    Clean up for production

--- allrequires.js
+++
@@ -1,7 +0,0 @@
-var Datacenter = require('./models/datacenter.js'),
-    Rack = require('./models/rack.js'),
-    Optionsdb = require('./models/options.js'),
-    Equipment = require('./models/equipment.js'),
-    System = require('./models/system.js'),
-    strTgs = require('./lib/stringThings.js'),
-    ObjectId = require('mongoose').Types.ObjectId;
