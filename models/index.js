581429b0f007ffde6a82efb92f9a6312ce5905c1
HyperSprite <chris-github@hypersprite.com>
2015-12-10 09:09:01 -0800

    syncup menues

---
+++ models/index.js
@@ -0,0 +1,17 @@
+const Datacenter = require('./datacenter');
+const Equipment = require('./equipment');
+const Fileinfo = require('./fileinfo');
+const Optionsdb = require('./options');
+const Rack = require('./rack');
+const Systemdb = require('./system');
+const User = require('./user');
+
+module.exports = {
+  Datacenter: Datacenter,
+  Equipment: Equipment,
+  Fileinfo: Fileinfo,
+  Optionsdb: Optionsdb,
+  Rack: Rack,
+  Systemdb: Systemdb,
+  User: User,
+};
