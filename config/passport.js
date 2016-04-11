e95959e5187b1f9bb5fbf9ee5f501a45304cbe7e
HyperSprite <chris-github@hypersprite.com>
2016-04-10 18:44:13 -0700

    some file cleanup

--- config/passport.js
+++ config/passport.js
@@ -1,198 +1,128 @@
-var logger = require('../lib/logger.js'),
-       strTgs = require('../lib/stringThings.js'),
-    accConfig = require('../config/access'),
-    Optionsdb = require('../models/options.js');
-
-// load all the things we need
-var LocalStrategy   = require('passport-local').Strategy;
-
-// load up the user model
-var User            = require('../models/user');
-
-
-/*
-exports.accessCheck = function(check){
-var access = new Object();
-// adjust this to change default action. 1 for everyone to login, even to read. 2 to allow for read only for anyone.
-if(!check){check.access=2;}
-if(check.access > 4){access.root = 1;}
-if(check.access > 3){access.delete = 1;}
-if(check.access > 2){access.edit = 1;}
-if(check.access > 1){access.read = 1;}
-if(check.access === 1){access.noAccess = 1;}
-return access;
-};
-*/
+const logger = require('../lib/logger.js');
+const strTgs = require('../lib/stringThings.js');
+const accConfig = require('../config/access');
+const Optionsdb = require('../models/options.js');
+const LocalStrategy = require('passport-local').Strategy;
+const User = require('../models/user');
 
 // expose this function to our app using module.exports
-module.exports = function(passport) {
+module.exports = (passport) => {
+// required for persistent login sessions
+// passport needs ability to serialize and unserialize users out of session
+// used to serialize the user for the session
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
 
-    // =========================================================================
-    // passport session setup ==================================================
-    // =========================================================================
-    // required for persistent login sessions
-    // passport needs ability to serialize and unserialize users out of session
-
-    // used to serialize the user for the session
-    passport.serializeUser(function(user, done) {
-        done(null, user.id);
+  // used to deserialize the user
+  passport.deserializeUser((id, done) => {
+    User.findById(id, (err, user) => {
+      done(err, user);
     });
+  });
 
-    // used to deserialize the user
-    passport.deserializeUser(function(id, done) {
-        User.findById(id, function(err, user) {
-            done(err, user);
-        });
-    });
-
-    // =========================================================================
-    // LOCAL SIGNUP ============================================================
-    // =========================================================================
-    // we are using named strategies since we have one for login and one for signup
-    // by default, if there was no name, it would just be called 'local'
-
-    passport.use('local-signup', new LocalStrategy({
-        // by default, local strategy uses username and password, we will override with email
-        usernameField : 'email',
-        passwordField : 'password',
-        passReqToCallback : true // allows us to pass back the entire request to the callback
-    },
-    function(req, email, password, done) {
-        // DefaultAcess sets the default level for a new user.
-        // All access is addeditive
-        // 1 = Limited
-        // 2 = Read access
-        // 3 = Edit access
-        // 4 = Delete access
-        // 5 = All access
-        var defaultAcess = 1;
-        // asynchronous
-        // User.findOne wont fire unless data is sent back
-        process.nextTick(function() {
-        // Check to see if User db exists, 
-        // if not, set the first user to level 5 access (root)
-        User.find(function(err, users){
-            if(!users.length){
-                defaultAcess = 5;
-                logger.warn('New Local Users db created');
-            }
-        });
-        // find a user whose email is the same as the forms email
-        // we are checking to see if the user trying to login already exists
-        User.findOne({ 'local.email' :  email }, function(err, user) {
-            // if there are any errors, return the error
-            if (err)
-                return done(err);
-
-            // check to see if theres already a user with that email
-            if (user) {
-                logger.warn('Email taken,'+email);
-                return done(null, false, req.flash('signupMessage', 'That email is already taken.'));
-                
-            } else {
-                
-                // if there is no user with that email
-                // create the user
-                var newUser            = new User();
-
-                // set the user's local credentials
-                newUser.local.email    = email;
-                newUser.local.password = newUser.generateHash(password);
-                newUser.access = defaultAcess;
-                newUser.createdOn = Date.now();
-                logger.warn(email+' created with acess level '+defaultAcess);
-
-                // save the user
-                newUser.save(function(err) {
-                    if (err)
-                        throw err;
-
-                    return done(null, newUser);
-                });
-            }
-
-        });    
-
-        });
-
-    }));
-// logger.info(local.email + ' local account already exists');
-// logger.info(local.email + ' new local account created');
-
-    // =========================================================================
-    // LOCAL LOGIN =============================================================
-    // =========================================================================
-    // we are using named strategies since we have one for login and one for signup
-    // by default, if there was no name, it would just be called 'local'
-
-    passport.use('local-login', new LocalStrategy({
-        // by default, local strategy uses username and password, we will override with email
-        usernameField : 'email',
-        passwordField : 'password',
-        passReqToCallback : true // allows us to pass back the entire request to the callback
-    },
-    function(req, email, password, done) { // callback with email and password from our form
-
-        // find a user whose email is the same as the forms email
-        // we are checking to see if the user trying to login already exists
-        User.findOne({ 'local.email' :  email }, function(err, user) {
-            // if there are any errors, return the error before anything else
-            if (err){
-                return done(err);
-            // if no user is found, return the message
-            }else if (!user){
-                logger.warn('ACCESS FAILED,'+email);
-                return done(null, false, req.flash('loginMessage', 'Opps! Login Failed.')); // req.flash is the way to set flashdata using connect-flash
-
-            // if the user is found but the password is wrong
-            }else if (!user.validPassword(password)){
-               logger.warn('PASSWORD FAILED,'+email);
-                return done(null, false, req.flash('loginMessage', 'Oops! Failed Login.')); // create the loginMessage and save it to session as flashdata
-
-            // all is well, return successful user
-           }else{
-            logger.info(user.local.email+' in time zone '+req.body.timezone);
-            req.session.ses = {
-                timezone : req.body.timezone,
-                access : accConfig.accessCheck(user),
-
-            };
-            return done(null, user);
-        
+  passport.use('local-signup', new LocalStrategy({
+  // by default, local strategy uses username and password, we will override with email
+    usernameField: 'email',
+    passwordField: 'password',
+    passReqToCallback: true, // allows us to pass back the entire request to the callback
+  }, (req, email, password, done) => {
+    // DefaultAccess sets the default level for a new user.
+    // All access is addeditive
+    // 1 = Limited
+    // 2 = Read access
+    // 3 = Edit access
+    // 4 = Delete access
+    // 5 = All access
+    var defaultAccess = 1;
+    // asynchronous
+    // User.findOne wont fire unless data is sent back
+    process.nextTick(() => {
+      // Check to see if User db exists,
+      // if not, set the first user to level 5 access (root)
+      User.find((err, users) => {
+        if (!users.length) {
+          defaultAccess = 5;
+          logger.warn('New Local Users db created');
         }
+      });
+      // find a user whose email is the same as the forms email
+      // we are checking to see if the user trying to login already exists
+      User.findOne({ 'local.email': email }, (err, user) => {
+        // if there are any errors, return the error
+        if (err) return done(err);
+        // check to see if theres already a user with that email
+        if (user) {
+          logger.warn(`Email taken,${email}`);
+          return done(null, false, req.flash('signupMessage', 'That email is already taken.'));
+        }
+        // if there is no user with that email
+        // create the user
+        var newUser = new User();
+        // set the user's local credentials
+        newUser.local.email = email;
+        newUser.local.password = newUser.generateHash(password);
+        newUser.access = defaultAccess;
+        newUser.local.createdOn = Date.now();
+        logger.warn(`${email} created with acess level ${defaultAccess}`);
+        // save the user
+        newUser.save((err) => {
+          if (err) throw err;
+          return done(null, newUser);
         });
-
-    }));
-
+      });
+    });
+  }));
+  // logger.info(local.email + ' local account already exists');
+  // logger.info(local.email + ' new local account created');
+  passport.use('local-login', new LocalStrategy({
+    // by default, local strategy uses username and password, we will override with email
+    usernameField: 'email',
+    passwordField: 'password',
+    passReqToCallback: true, // allows us to pass back the entire request to the callback
+  }, (req, email, password, done) => { // callback with email and password from our form
+    // find a user whose email is the same as the forms email
+    // we are checking to see if the user trying to login already exists
+    User.findOne({ 'local.email': email }, (err, user) => {
+      // if there are any errors, return the error before anything else
+      if (err) {
+        return done(err);
+          // if no user is found, return the message
+      } else if (!user) {
+        logger.warn(`ACCESS FAILED, ${email}`);
+        return done(null, false, req.flash('loginMessage', 'Opps! Login Failed.')); // req.flash is the way to set flashdata using connect-flash
+         // if the user is found but the password is wrong
+      } else if (!user.validPassword(password)) {
+        logger.warn(`PASSWORD FAILED,${email}`);
+        return done(null, false, req.flash('loginMessage', 'Oops! Failed Login.')); // create the loginMessage and save it to session as flashdata
+          // all is well, return successful user
+      }
+      logger.info(`${user.local.email} in time zone ${req.body.timezone}`);
+      req.session.ses = {
+        timezone: req.body.timezone,
+        access: accConfig.accessCheck(user),
+      };
+      return done(null, user);
+    });
+  }));
 };
-/*
-req.session.sesUser :{
-    timezone : timezone,
-    user : {
-            authId :  string,
-            password : string,
-            name : string,
-            email : string,
-            phone : string,
-            createdOn : date,
-            lastAccessed : date,
-        },
-    access : {
-        root :
-        delete :
-        edit :
-        read :
-        noaccess : 
-        }
 
-
-    }
-    }
-
-
-}
-
-
-
-
-*/
+// req.session.sesUser: {
+//   timezone : timezone,
+//   user : {
+//     authId :  string,
+//     password : string,
+//     name : string,
+//     email : string,
+//     phone : string,
+//     createdOn : date,
+//     lastAccessed : date,
+//   },
+//   access : {
+//     root :
+//     delete :
+//     edit :
+//     read :
+//     noaccess :
+//   }
+// }
--- handlers/user.js
+++ handlers/user.js
@@ -1,65 +1,62 @@
 // users
-var     logger = require('../lib/logger.js'),
-        strTgs = require('../lib/stringThings.js'),
-      ObjectId = require('mongoose').Types.ObjectId,
-      passport = require('passport');
+const logger = require('../lib/logger.js');
+const strTgs = require('../lib/stringThings.js');
+const ObjectId = require('mongoose').Types.ObjectId;
+const passport = require('passport');
+const Users = require('../models/user.js');
+const Optionsdb = require('../models/options.js');
+const VALID_EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)+$/;
 
-// Models
-     var Users = require('../models/user.js'),
-     Optionsdb = require('../models/options.js');
-
-exports.home = function(req, res){
-//    logger.info(req.params.data);
-//    logger.info('req.headers.referer'+req.headers.referer);
-    if(!req.params.data){
-    res.render ('user/home');
-    } else if (req.params.data === 'login'){
-        res.render ('user/login');
-    } else if (req.params.data === 'signup'){
-        res.render ('user/signup');
-    } else if (req.params.data === 'profile'){
-        res.render('user/profile', {
-        user : req.user // get the user out of session and pass to template
-    });     
-    } else if (req.params.data === 'logout'){
-        req.logout();
-        res.redirect('/');
-    }
+exports.home = (req, res) => {
+  // logger.info(req.params.data);
+  // logger.info('req.headers.referer'+req.headers.referer);
+  if (!req.params.data) {
+    res.render('user/home');
+  } else if (req.params.data === 'login') {
+    res.render('user/login');
+  } else if (req.params.data === 'signup') {
+    res.render('user/signup');
+  } else if (req.params.data === 'profile') {
+    res.render('user/profile', {
+      user: req.user, // get the user out of session and pass to template
+    });
+  } else if (req.params.data === 'logout') {
+    req.logout();
+    res.redirect('/');
+  }
 };
 
 
-var VALID_EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)+$/;
-exports.localSignup = function(req,res, next){
-        passport.authenticate('local-signup', function(err, user, info) {
-        if (err) {return next(err); 
-        }
-        if (!user) {
-            req.session.flash = {
-            type: 'danger',
-            intro: 'Signup Failed!',
-            message: 'Your emial address or password are not valid or is a duplicate!',
-            };
-        return res.redirect(303, '/user/signup');
-        }
-        req.logIn(user, function(err) {
-        if (err) { return next(err); }
-        return res.redirect(303, '/user/profile');
+exports.localSignup = (req, res, next) => {
+  passport.authenticate('local-signup', (err, user, info) => {
+    if (err) return next(err);
+    if (!user) {
+      req.session.flash = {
+        type: 'danger',
+        intro: 'Signup Failed!',
+        message: 'Your emial address or password are not valid or is a duplicate!',
+      };
+      return res.redirect(303, '/user/signup');
+    }
+    req.logIn(user, (err) => {
+      if (err) { return next(err); }
+      return res.redirect(303, '/user/profile');
     });
   })(req, res, next);
 };
 
-exports.localLogin = function(req, res, next) {
-  passport.authenticate('local-login', function(err, user, info) {
+exports.localLogin = (req, res, next) => {
+  passport.authenticate('local-login', (err, user, info) => {
     if (err) { return next(err); }
     if (!user) {
-                    req.session.flash = {
-                    type: 'danger',
-                    intro: 'Login Failed!',
-                    message: 'Username or password not valid!',
-                    };
-                return res.redirect(303, '/');
-}
-    req.logIn(user, function(err) {
+      req.session.flash = {
+        type: 'danger',
+        intro: 'Login Failed!',
+        message: 'Username or password not valid!',
+      };
+      return res.redirect(303, '/');
+    }
+    req.logIn(user, (err) => {
       if (err) { return next(err); }
       return res.redirect(303, req.body.requrl);
     });
